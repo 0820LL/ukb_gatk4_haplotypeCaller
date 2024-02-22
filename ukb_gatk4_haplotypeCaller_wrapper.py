81000b16e965664cbef268e56bde2610bc89b9d3
lilin <lilin@ubuntu>
2024-02-22 13:55:47 +0800

    MQ返回的config.json文件中增加了 'pipeline':'ukb'

--- ukb_gatk4_haplotypeCaller_wrapper.py
+++ ukb_gatk4_haplotypeCaller_wrapper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import logging
 import os
 import json
 import time
@@ -16,32 +17,31 @@ def steward(config_file_path:str, ukb_gatk4_haplotypeCaller_path:str, send_messa
     # get paramters from the config.json
     with open(config_file_path, 'r') as config_f:
         config_d = json.load(config_f)
-    task_id            = config_d['taskId']
-    analysis_record_id = config_d['analysisRecordId']
     # make the params.json file
     params_d = {
-        'cram' : config_d['cram'],
-        'fasta': config_d['fasta'],
+        'cram' : config_d['ukbParams']['cram'],
+        'fasta': config_d['ukbParams']['fasta'],
     }
     params_file_path = '{}/params.json'.format(analysis_path)
     with open(params_file_path, 'w') as params_f:
         json.dump(params_d, params_f, ensure_ascii=False, indent=4)
     ukb_gatk4_haplotypeCaller_command = 'nextflow run -offline -profile singularity -bg -params-file {} {} >> run_log.txt'.format(params_file_path, ukb_gatk4_haplotypeCaller_path)
     return_value = os.system(ukb_gatk4_haplotypeCaller_command)
-    with open('ukb_gatk4_haplotypeCaller_command.txt', 'w') as command_f:
-        command_f.write(ukb_gatk4_haplotypeCaller_command + '\n')
-        command_f.write('return value:{}\n'.format(str(return_value)))
+    logging.info(ukb_gatk4_haplotypeCaller_command)
+    logging.info('return value:{}\n'.format(str(return_value)))
     time.sleep(10)
     # send the result files 
     feedback_dict = {
-        'tTaskId'         : task_id,
-        'analysisRecordId': analysis_record_id,
-        'pipelineName'    : 'ukb_gatk4_haplotypeCaller',
-        'analysisStatus'  : '',
-        'startDate'       : time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()),
-        'endDate'         : '',
-        'error'           : 0,
-        'taskName'        : 'Step'
+        'uuid'          : config_d['uuid'],
+        'ukbId'         : config_d['ukbId'],
+        'ukbToolsCode'  : config_d['ukbToolsCode'],
+        'ukbToolName'   : config_d['ukbToolName'],
+        'pipeline'      : 'ukb',
+        'analysisStatus': '',
+        'startDate'     : time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()),
+        'endDate'       : '',
+        'error'         : 0,
+        'taskName'      : 'Step'
     }
     if return_value == 0 and os.path.exists('{}/results'.format(analysis_path)):
         feedback_dict['analysisStatus'] = '分析开始'
@@ -129,8 +129,15 @@ def main() -> None:
     parser.add_argument('--send_message_script', required=True, help='the full path for the shell script: sendMessage.sh')
     args = parser.parse_args()
     config_file_path = args.cfp
+    if os.path.isabs(config_file_path):
+        pass
+    else:
+        config_file_path = os.path.abspath(os.path.basename(config_file_path))
     ukb_gatk4_haplotypeCaller_path = args.ukb_gatk4_haplotypeCaller_path
     send_message_script = args.send_message_script
+    # logging
+    log_file = '{}/ukb_gatk4_haplotypeCaller.log'.format(os.path.dirname(config_file_path))
+    logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     steward(config_file_path, ukb_gatk4_haplotypeCaller_path, send_message_script)
 
 
